b465a9fda72d1016e9faf214729b95ae944560fe
hexingMagus <elllliotttt@gmail.com>
2023-03-01 13:18:32 -0800

    makes the lines work
    they properly connect to where the notes are now (and are colored)

--- NoteBar.gd
+++ NoteBar.gd
@@ -3,8 +3,17 @@ extends Node2D
 # Declare member variables here. Examples:
 var time = 0
 var bpm = 100
-var beat = 2
-onready var noteBar = $NoteBar
+var beat = 1
+var lastBeat = 0
+var line
+onready var NB1 = $NoteBar
+onready var NB2 = $NoteBar2
+onready var NB3 = $NoteBar3
+onready var NB4 = $NoteBar4
+onready var NB5 = $NoteBar5
+onready var NB6 = $NoteBar6
+onready var NB7 = $NoteBar7
+onready var NB8 = $NoteBar8
 const note = preload("res://Note.tscn")
 
 
@@ -14,13 +23,95 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	noteBar.position.y = noteBar.position.y + delta*100
+	NB1.position.y = NB1.position.y + delta*100
+	NB2.position.y = NB2.position.y + delta*100
+	NB3.position.y = NB3.position.y + delta*100
+	NB4.position.y = NB4.position.y + delta*100
+	NB5.position.y = NB5.position.y + delta*100
+	NB6.position.y = NB6.position.y + delta*100
+	NB7.position.y = NB7.position.y + delta*100
+	NB8.position.y = NB8.position.y + delta*100
 
 	time += delta*100
 	if (time > bpm*(beat-1)):
-		var notePosition = Vector2(((beat^2)*100)%1000, -beat*100)
-		noteBar.add_point(notePosition)
-		var noteCircle = note.instance()
-		noteCircle.position = notePosition
-		noteBar.add_child(noteCircle)
+		var notes = Array()
+		for _i in range(1,9):
+			randomize()
+			notes.append(randi()%2)
+		addNotes(notes, beat)
 		beat = beat + 1
+		
+func addNotes(lanes, beat):
+	wire4(lanes, beat)
+			
+func wire4(lanes, beat):
+	
+	for i in range(0, lanes.size()):
+		if lanes[i] == 1:
+			var notePosition = Vector2(i*100 + 125 + rand_range(0, 50), -beat*100)
+			var noteCircle = note.instance()
+			noteCircle.position = notePosition
+			if i == 0:
+				NB1.add_child(noteCircle)
+				NB1.add_point(notePosition)
+				if lanes[1] == 0:
+					NB2.add_point(notePosition)
+					if lanes[2] == 0 && lanes[3] == 0:
+						NB3.add_point(notePosition)
+						NB4.add_point(notePosition)
+			elif i == 1:
+				NB2.add_child(noteCircle)
+				NB2.add_point(notePosition)
+				if lanes[0] == 0:
+					NB1.add_point(notePosition)
+				if lanes[2] == 0 && lanes[3] == 0:
+					NB3.add_point(notePosition)
+					NB4.add_point(notePosition)
+			elif i == 2:
+				NB3.add_child(noteCircle)
+				NB3.add_point(notePosition)
+				if lanes[3] == 0:
+					NB4.add_point(notePosition)
+				if lanes[0] == 0 && lanes[1] == 0:
+					NB1.add_point(notePosition)
+					NB2.add_point(notePosition)
+			elif i == 3:
+				NB4.add_child(noteCircle)
+				NB4.add_point(notePosition)
+				if lanes[2] == 0:
+					NB3.add_point(notePosition)
+					if lanes[0] == 0 && lanes[1] == 0:
+						NB1.add_point(notePosition)
+						NB2.add_point(notePosition)
+			elif i == 4:
+				NB5.add_child(noteCircle)
+				NB5.add_point(notePosition)
+				if lanes[5] == 0:
+					NB6.add_point(notePosition)
+					if lanes[6] == 0 && lanes[7] == 0:
+						NB7.add_point(notePosition)
+						NB8.add_point(notePosition)
+			elif i == 5:
+				NB6.add_child(noteCircle)
+				NB6.add_point(notePosition)
+				if lanes[4] == 0:
+					NB5.add_point(notePosition)
+				if lanes[6] == 0 && lanes[7] == 0:
+					NB7.add_point(notePosition)
+					NB8.add_point(notePosition)
+			elif i == 6:
+				NB7.add_child(noteCircle)
+				NB7.add_point(notePosition)
+				if lanes[7] == 0:
+					NB8.add_point(notePosition)
+				if lanes[4] == 0 && lanes[5] == 0:
+					NB5.add_point(notePosition)
+					NB6.add_point(notePosition)
+			elif i == 7:
+				NB8.add_child(noteCircle)
+				NB8.add_point(notePosition)
+				if lanes[6] == 0:
+					NB7.add_point(notePosition)
+					if lanes[4] == 0 && lanes[5] == 0:
+						NB5.add_point(notePosition)
+						NB6.add_point(notePosition)
